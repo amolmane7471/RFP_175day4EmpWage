316f4feeb82e266d03b8b6ea4613d8ae37559479
amolmane7471 <maneamol3233@gmail.com>
2022-08-09 14:12:52 +0530

    [Amol]:added a employee wage program

---
+++ src/com/bridgelabz/empwage/EmpWage.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.empwage;
+
+public class EmpWage {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation Pgm");
+		
+	}
+
+}
